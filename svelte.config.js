130b1315c56ce087c35da187526d3b1581a439e0
Vitaly Pavlenko <cxielamiko@gmail.com>
2023-09-06 22:39:16 +0400

    Try to fix vercel.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,26 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
-const dev = process.argv.includes('dev')
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	vitePlugin: {
-		inspector: true,
-	},
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html',
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
-	}
-}
-export default config
+  vitePlugin: {
+    inspector: true,
+  },
+  kit: {
+    adapter: adapter({
+      fallback: "index.html",
+      pages: "build",
+      assets: "build",
+    }),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+  },
+};
+export default config;
